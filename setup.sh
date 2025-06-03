0287c03f08650d9936364dc8c7493d607a1edc37
abdullah imran <abdullahimran@gmail.com>
2025-06-03 15:26:58 +0500

    initial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git init 
+git config --local user.name "abdullah imran"
+git config --local user.email "abdullahimran@gmail.com"
+git config --local core.editor notepad
+git branch -m develop
+mkdir ./src ./styles ./asssts ./bn ./modules
+touch ./src/index.html ./styles/index.css
+touch .gitignore
+git remote add origin https://github.com/Abran-006/s2week11practice.git
